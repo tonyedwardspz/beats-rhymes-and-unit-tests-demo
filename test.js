bfbcee489430b427734967282cacbde048ec9c3b
Tony Edwards <tony@plymouthsoftware.com>
2018-02-17 02:22:20 +0000

    Adds placeholder tests

---
+++ test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Basic test to enable initial CI
+var assert = require('assert');
+describe('Sanity', function() {
+
+  describe('#doTestsRun()', function() {
+    it('should return -1 when there is some semblance of sanity in JavaScript land', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
